666e662bf6aa0bd439acac5c87462eef1b7f1b64
Robert Christensen <robert.christensen@mymail.champlain.edu>
2021-11-01 17:37:37 -0400

    Moved damage numbers to CombatLogger

--- Mutexing Ability Combat/Assets/Combat/Enemy.cs
+++ Mutexing Ability Combat/Assets/Combat/Enemy.cs
@@ -11,36 +11,5 @@ public class Enemy : Combatant
         return false; //TODO
     }
 
-    [Header("Messages")]
-    [SerializeField] private TextMessage damageNumberPrefab;
-    [SerializeField] private Transform   damageNumberSource;
-    [SerializeField] private float       damageNumberLife;
-
-    protected override void HandleEvent(DamageEvent damage)
-    {
-        if (damage.target == this)
-        {
-            base.HandleEvent(damage);
-
-            //TODO move to separate script?
-            TextMessage obj = Instantiate(damageNumberPrefab.gameObject, damageNumberSource.position, damageNumberSource.rotation).GetComponent<TextMessage>();
-            obj.uiText.text = damage.postMitigation.ToString();
-            obj.lifetime = damageNumberLife;
-        }
-    }
-
-    protected override void HandleEvent(DeathEvent death)
-    {
-        if(death.target == this)
-        {
-            //TODO move to separate script?
-            TextMessage obj = Instantiate(damageNumberPrefab.gameObject, damageNumberSource.position, damageNumberSource.rotation).GetComponent<TextMessage>();
-            obj.uiText.text = death.target.GetDisplayName() + " killed by " + death.source.GetDisplayName();
-            obj.lifetime = damageNumberLife;
-            
-            base.HandleEvent(death);
-        }
-    }
-
     public override string GetDisplayName() => "Enemy";
 }
